32a34a10dd2d1f5a9a0302f64938d3179af78edd
AkshayWankhade23 <akshaywankhade664@gmail.com>
2024-01-17 00:29:57 +0530

    Created server and client folder

---
+++ server/models/user.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose");
+
+// Define User Schema
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+});
+
+//Create and export user model
+module.exports = mongoose.model("User", userSchema);
